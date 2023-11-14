c65a02c586db00af5a3ea3ebc39014c626f9f6e0
razorpro9991 <145159604+razorpro9991@users.noreply.github.com>
2023-11-14 19:55:16 +0000

    Update printx_unsi.c

--- printx_unsi.c
+++ printx_unsi.c
@@ -1,47 +1,48 @@
 #include "main.h"
 
 /**
- * printx_unsi- Prints an unsigned int.
+ * printx_unsi - Prints an unsigned int.
  * @args: Argument to print.
- * Nabil & Eben
  *
- * Return: Number of char printed.
+ * Return: Number of characters printed.
  */
 int printx_unsi(va_list args)
 {
-	unsigned int p = va_arg(args, unsigned int);
-	int number, last = p % 10, digit, exp = 1;
-	int j = 1;
+    unsigned int p = va_arg(args, unsigned int);
+    int number, last = p % 10, digit, exp = 1;
+    int j = 1;
 
-	p = p / 10;
-	number = p;
+    p = p / 10;
+    number = p;
 
-	if (last < 0)
-	{
-		_putchar('-');
-		number = -number;
-		p = -p;
-		last = -last;
-		j++;
-	}
-	if (number > 0)
-	{
-		while (number / 10 != 0)
-		{
-			exp = exp * 10;
-			number = number / 10;
-		}
-		number = p;
-		while (exp > 0)
-		{
-			digit = number / exp;
-			_putchar(digit + '0');
-			number = number - (digit * exp);
-			exp = exp / 10;
-			j++;
-		}
-	}
-	_putchar(last + '0');
+    if (p < 0) // Corrected condition
+    {
+        _putchar('-');
+        number = -number;
+        p = -p;
+        last = -last;
+        j++;
+    }
 
-	return (j);
+    if (number > 0)
+    {
+        while (number / 10 != 0)
+        {
+            exp = exp * 10;
+            number = number / 10;
+        }
+        number = p;
+        while (exp > 0)
+        {
+            digit = number / exp;
+            _putchar(digit + '0');
+            number = number - (digit * exp);
+            exp = exp / 10;
+            j++;
+        }
+    }
+
+    _putchar(last + '0');
+
+    return (j);
 }
