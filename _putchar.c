64f3c8337ccd116452f4339d63fc902013bf3f01
razorpro9991 <145159604+razorpro9991@users.noreply.github.com>
2023-11-14 00:19:53 +0000

    Update _putchar.c

--- _putchar.c
+++ _putchar.c
@@ -3,7 +3,7 @@
 /**
  * _putchar - Writes a single character to the standard output.
  * @c: The character to be printed.
- *
+ * Author: Nabil and Eben
  * Return: returns 1. On error, returns -1 and sets errno accordingly.
  */
 int _putchar(char c)
