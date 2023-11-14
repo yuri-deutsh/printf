4cea66d49d5ce9f10ac4f5befc2bf1f1bd2c67d8
razorpro9991 <145159604+razorpro9991@users.noreply.github.com>
2023-11-14 19:52:23 +0000

    Update printx_unsi.c

--- printx_unsi.c
+++ printx_unsi.c
@@ -1,48 +1,30 @@
 #include "main.h"
 
 /**
- * printx_unsi- Prints an unsigned int.
- * @args: Argument to print.
- * Nabil & Eben
- *
- * Return: Number of char printed.
+ * printx_unsi - Prints an unsigned number
+ * Author: Nabil & Eben
+ * Return: Number of chars printed.
  */
-int printx_unsi(va_list args)
+int printx_unsi(va_list types, char buffer[],
+	int flags, int width, int precision, int size)
 {
-	unsigned int p = va_arg(args, unsigned int);
-	int number, last = p % 10, digit, exp = 1;
-	int j = 1;
+	int y = BUFF_SIZE - 2;
+	unsigned long int num = va_arg(types, unsigned long int);
 
-	p = p / 10;
-	number = p;
+	num = convert_size_unsgnd(num, size);
 
-	if (last < 0)
+	if (num == 0)
+		buffer[y--] = '0';
+
+	buffer[BUFF_SIZE - 1] = '\0';
+
+	while (num > 0)
 	{
-		_putchar('-');
-		number = -number;
-		p = -p;
-		last = -last;
-		j++;
+		buffer[y--] = (num % 10) + '0';
+		num /= 10;
 	}
-	if (number > 0)
-	{
-		while (number / 10 != 0)
-		{
-			exp = exp * 10;
-			number = number / 10;
-		}
-		number = p;
-		while (exp > 0)
-		{
-			digit = number / exp;
-			_putchar(digit + '0');
-			number = number - (digit * exp);
-			exp = exp / 10;
-			j++;
-		}
-	}
-	_putchar(last + '0');
 
-	return (j);
+	y++;
+
+	return (write_unsgnd(0, y, buffer, flags, width, precision, size));
 }
-
